486409e2f719e593c79112f2a5455f9354eeab9a
hixsav <hi.savis@gmail.com>
2018-08-02 00:08:00 +0100

    Updated components

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -2,7 +2,57 @@ import React, { Component } from 'react';
 
 class Register extends Component {
 	render() {
-		return <div>Register</div>;
+		return (
+			<div className="register">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-8 m-auto">
+							<h1 className="display- text-center">Sign up</h1>
+							<p className="lead-1 text-center"> Register to Create a Profile</p>
+							<form action="signup.html">
+								<div className="form-group">
+									<input
+										type="text"
+										class="form-control-form-control-lg"
+										placeholder="Name"
+										name="name"
+										required
+									/>{' '}
+								</div>
+								<div className="form-group">
+									<input
+										type="email"
+										class="form-control-form-control-lg"
+										placeholder="Email Address"
+										name="email"
+									/>
+									<small className="form-text text-muted">
+										This site uses Gravatar so if you want a profile image, use a Gravatar email
+									</small>
+								</div>
+								<div className="form-group">
+									<input
+										type="password"
+										class="form-control-form-control-lg"
+										placeholder="Password"
+										name="password"
+									/>
+								</div>
+								<div className="form-group">
+									<input
+										type="password"
+										class="form-control-form-control-lg"
+										placeholder="Confirm Password"
+										name="password2"
+									/>
+								</div>
+								<input type="submit" class="btn btn-info btn-block mt-4" />
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
